8640b0f1527881ce20559b504756d827cca254c3
Sergio E <ses110@gmail.com>
2021-05-18 12:55:50 -0500

    feat: lockProvider uses LockRepository to send lock action

--- lib/providers/lock_provider.dart
+++ lib/providers/lock_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:yonomi_platform_sdk/repository/devices/devices_repository.dart';
+import 'package:yonomi_platform_sdk/repository/devices/lock_repository.dart';
 import 'package:yonomi_platform_sdk/request/request.dart';
 
 class LockProvider extends ChangeNotifier {
+  bool loading = false;
+
   LockProvider(Request request, String deviceId) {
     _request = request;
     getDeviceDetail(deviceId);
@@ -13,8 +16,17 @@ class LockProvider extends ChangeNotifier {
 
   Device get deviceDetail => _deviceDetail;
 
+  bool get isLocked => _deviceDetail?.traits?.first?.state?.value ?? false;
+
   Future<void> getDeviceDetail(String deviceId) async {
+    loading = true;
     _deviceDetail = await DevicesRepository.getLockDetails(_request, deviceId);
+
+    loading = false;
     notifyListeners();
+  }
+
+  Future<void> setLockUnlockAction(String deviceId, bool setLock) async {
+    await LockRepository.sendLockUnlockAction(_request, deviceId, setLock);
   }
 }
