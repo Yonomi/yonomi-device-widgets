f80b2ab817eeb8fdff7e06fc1344613ad7b63d7b
Sergio E <ses110@gmail.com>
2021-05-18 12:53:36 -0500

    refactor: rename ColorConstants class to WidgetStyleConstants

---
+++ lib/ui/widget_style_constants.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class WidgetStyleConstants {
+  static const Color modesButtonUnpressedColor = Color(0xFFDFE0E4);
+
+  static const Color modesButtonPressedColor = Color(0xFF5D657A);
+
+  static const Color deviceIconColor = Color(0xFF1E1F29);
+
+  static const Color textColorState = Color(0xFF00B3A6);
+
+  static TextStyle deviceItemTextPrimaryState = TextStyle(
+      fontWeight: FontWeight.w900,
+      color: WidgetStyleConstants.textColorState,
+      fontSize: 19.0);
+}
