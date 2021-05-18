c7732fe750e233667df71fd5abcb1b03776be7fe
Sergio E <ses110@gmail.com>
2021-05-18 13:00:24 -0500

    refactor: move DeviceItemIcon mapper class into device-widgets package

---
+++ lib/assets/traits/device_item_icon.dart
@@ -0,0 +1,52 @@
+import 'package:device_widgets/ui/widget_style_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:yonomi_platform_sdk/repository/devices/devices_repository.dart';
+
+class DeviceItemIcon {
+  static Widget getIcon(List<Trait> traits) {
+    Trait determiningTrait = traits[0];
+    if (determiningTrait is LockUnlockTrait) {
+      return (determiningTrait.state.value)
+          ? getLockIcon(60)
+          : getUnlockIcon(60);
+    }
+    if (determiningTrait is ThermostatTrait) {
+      return getThermostatIcon(determiningTrait.state.value);
+    }
+    return Icon(
+      Icons.device_unknown,
+      size: 60,
+      color: WidgetStyleConstants.deviceIconColor,
+    );
+  }
+
+  static Widget getLockIcon(double size) {
+    return Icon(
+      Icons.lock,
+      size: size,
+      color: WidgetStyleConstants.deviceIconColor,
+    );
+  }
+
+  static Widget getUnlockIcon(double size) {
+    return Icon(
+      Icons.lock_open,
+      size: size,
+      color: WidgetStyleConstants.deviceIconColor,
+    );
+  }
+
+  static Widget getThermostatIcon(double lockState) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          border: Border.all(width: 3, color: Colors.black)),
+      child: Center(
+        child: Text(
+          (lockState != null) ? lockState.round().toString() : 'N/A',
+          style: WidgetStyleConstants.deviceItemTextPrimaryState,
+        ),
+      ),
+    );
+  }
+}
