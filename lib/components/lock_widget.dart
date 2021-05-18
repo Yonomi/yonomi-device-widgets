12dba33074c4f59ebdc7b5f3c16eb0d288505b66
Sergio E <ses110@gmail.com>
2021-05-18 12:56:31 -0500

    feat: use Arc component for Lock Widget

--- lib/components/lock_widget.dart
+++ lib/components/lock_widget.dart
@@ -1,3 +1,4 @@
+import 'package:device_widgets/components/arc.dart';
 import 'package:device_widgets/providers/lock_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,6 +7,7 @@ class LockWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final lockProvider = Provider.of<LockProvider>(context, listen: true);
+
     return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
       Row(
         children: <Widget>[
@@ -19,7 +21,19 @@ class LockWidget extends StatelessWidget {
         height: 80,
       ),
       Center(
-        child: Text("Lock component will go here"),
+        child: Arc(
+          centerWidget: Text(
+            lockProvider.isLocked ? "Locked" : "Unlocked",
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          initialValue: 0.0,
+          onFinalSetPoint: (double value) {
+            bool setLock = value != 0;
+            lockProvider.setLockUnlockAction(
+                lockProvider?.deviceDetail?.id, setLock);
+          },
+          maxValue: 1,
+        ),
       ),
     ]);
   }
