d088a5cb7fd65b7a6d9327b7f96a3e2bc635b9d6
Sergio E <ses110@gmail.com>
2021-12-06 13:47:50 -0600

    feat(power_trait_provider.dart): introduce PowerTraitProvider

---
+++ lib/providers/power_trait_provider.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/widgets.dart';
+import 'package:yonomi_device_widgets/devices/trait_based_device_screen.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
+
+typedef GetDeviceDetailsMethod = Future<Device> Function(
+    Request request, String id);
+
+typedef SendPowerMethod = Future<void> Function(
+    Request request, String id, bool onOff);
+
+class PowerTraitProvider extends ChangeNotifier {
+  final int MAX_RETRIES = 10;
+
+  bool _isLoading = false;
+
+  bool _isPerformingAction = false;
+
+  late String _deviceId;
+
+  late Request _request;
+
+  Device? _deviceDetail;
+
+  Device? get deviceDetail => _deviceDetail;
+
+  set deviceDetail(Device? detail) {
+    _deviceDetail = detail;
+
+    setLoading = false;
+  }
+
+  PowerTraitProvider(Request request, String deviceId,
+      {GetDeviceDetailsMethod getDetails =
+          DevicesRepository.getDeviceDetails}) {
+    _request = request;
+    _deviceId = deviceId;
+    fetchData(getDeviceDetails: getDetails);
+  }
+
+  Future<Device?> fetchData(
+      {GetDeviceDetailsMethod getDeviceDetails =
+          DevicesRepository.getDeviceDetails}) async {
+    setLoading = true;
+
+    this.deviceDetail = await getDeviceDetails(_request, _deviceId);
+
+    return _deviceDetail;
+  }
+
+  Future<void> sendPowerOnOffAction(bool setOnOff,
+      {GetDeviceDetailsMethod getDetails = DevicesRepository.getDeviceDetails,
+      SendPowerMethod sendPowerMethod =
+          PowerRepository.sendPowerAction}) async {
+    if (!isPerformingAction) {
+      setPerformingAction = true;
+
+      await sendPowerMethod(_request, this._deviceId, setOnOff);
+
+      int numRetries = 0;
+      while (getPowerTrait()?.state.value != setOnOff &&
+          numRetries < MAX_RETRIES) {
+        _deviceDetail = await getDetails(_request, _deviceId);
+
+        await Future.delayed(Duration(milliseconds: 750));
+        numRetries++;
+      }
+
+      setPerformingAction = false;
+    }
+  }
+
+  PowerTrait? getPowerTrait() {
+    return _deviceDetail?.traits.first as PowerTrait?;
+  }
+
+  set setLoading(bool newIsLoading) {
+    _isLoading = newIsLoading;
+    notifyListeners();
+  }
+
+  bool get isLoading => _isLoading;
+
+  set setPerformingAction(bool newIsPerformingAction) {
+    _isPerformingAction = newIsPerformingAction;
+    notifyListeners();
+  }
+
+  bool get isPerformingAction => _isPerformingAction;
+}
---
+++ test/providers/power_trait_provider_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:yonomi_device_widgets/devices/trait_based_device_screen.dart';
+import 'package:yonomi_device_widgets/providers/power_trait_provider.dart';
+import 'package:yonomi_platform_sdk/third_party/yonomi_graphql_schema/schema.docs.schema.gql.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
+
+import 'power_trait_provider_test.mocks.dart';
+
+class GetDeviceDetailsMethod extends Mock {
+  Future<Device> call(Request request, String id);
+}
+
+class SendPowerMethod extends Mock {
+  Future<void> call(Request request, String id, bool onOff);
+}
+
+@GenerateMocks([GetDeviceDetailsMethod, SendPowerMethod])
+void main() {
+  test('Calling sendOnOff calls repository method', () async {
+    String deviceId = 'aDeviceId';
+    Request request = Request("", {});
+    final mockDeviceDetailsMethod = MockGetDeviceDetailsMethod();
+    final device = Device(
+      deviceId,
+      'name',
+      'description',
+      'manufacturerName',
+      'model',
+      null,
+      GDateTime('value'),
+      GDateTime('value'),
+      [
+        PowerTrait('name', UnknownState()),
+      ],
+    );
+    when(mockDeviceDetailsMethod.call(request, deviceId))
+        .thenAnswer((_) => Future.value(device));
+    final mockSendPowerMethod = MockSendPowerMethod();
+    PowerTraitProvider powerProvider = await PowerTraitProvider(
+        request, deviceId,
+        getDetails: mockDeviceDetailsMethod);
+
+    await powerProvider.sendPowerOnOffAction(true,
+        getDetails: mockDeviceDetailsMethod,
+        sendPowerMethod: mockSendPowerMethod);
+
+    verify(mockDeviceDetailsMethod(request, deviceId)).called(greaterThan(0));
+
+    verify(mockSendPowerMethod(request, deviceId, true)).called(greaterThan(0));
+  });
+}
