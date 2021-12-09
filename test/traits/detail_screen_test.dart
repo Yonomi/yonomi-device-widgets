9ed8cab36f1fef67e613404bfbeaf9647cadf2a8
Sergio E <ses110@gmail.com>
2021-12-09 08:56:27 -0600

    test(detail_screen_test.dart): add missing type to a parameter and some refactoring

--- test/traits/detail_screen_test.dart
+++ test/traits/detail_screen_test.dart
@@ -29,32 +29,31 @@ Widget createDetailScreenWhenLoading(
 
   LockProvider mockLockProvider = MockLockProvider();
 
-  return MaterialApp(
-    home: Column(children: [
-      MultiProvider(providers: [
-        ChangeNotifierProvider<TraitBasedDeviceNotifier>.value(
-            value: mockTraitBasedNotifier),
-        ChangeNotifierProvider<LockProvider>.value(value: mockLockProvider),
-      ], child: DetailScreenWidget(req, deviceId)),
-    ]),
-  );
+  return createMaterialApp(
+      mockTraitBasedNotifier, mockLockProvider, req, deviceId);
 }
 
 Widget createDetailScreenWidgetForTrait(
-  Trait,
+  Trait trait,
   Request req,
   String deviceId,
 ) {
   TraitBasedDeviceNotifier mockTraitBasedNotifier =
       MockTraitBasedDeviceNotifier();
   when(mockTraitBasedNotifier.isLoading).thenReturn(false);
-  when(mockTraitBasedNotifier.deviceDetail).thenReturn(buildMockDevice(Trait));
+  when(mockTraitBasedNotifier.deviceDetail).thenReturn(buildMockDevice(trait));
 
   LockProvider mockLockProvider = MockLockProvider();
   when(mockLockProvider.loadingDetail).thenReturn(false);
   when(mockLockProvider.loadingAction).thenReturn(false);
   when(mockLockProvider.isLocked).thenReturn(false);
 
+  return createMaterialApp(
+      mockTraitBasedNotifier, mockLockProvider, req, deviceId);
+}
+
+MaterialApp createMaterialApp(TraitBasedDeviceNotifier mockTraitBasedNotifier,
+    LockProvider mockLockProvider, Request req, String deviceId) {
   return MaterialApp(
     home: Column(children: [
       MultiProvider(providers: [
