90cfe2cce08c857e733c10869ce8447c2a5ebef7
Sergio E <ses110@gmail.com>
2021-12-13 14:49:10 -0600

    feat(power_widget): handle loading, error and normal UI states for PowerWidget

--- test/traits/power_widget_test.dart
+++ test/traits/power_widget_test.dart
@@ -16,14 +16,71 @@ MaterialApp createMaterialApp(PowerTraitProvider mockPowerProvider) {
 
 @GenerateMocks([PowerTraitProvider])
 void main() {
-  testWidgets('When loading, should show CircularProgressIndicator ',
-      (WidgetTester tester) async {
-    final mockPowerTraitProvider = MockPowerTraitProvider();
-    when(mockPowerTraitProvider.isBusy).thenReturn(true);
-    // when(mockPowerTraitProvider.loadingAction).thenReturn(false);
-    await tester.pumpWidget(createMaterialApp(mockPowerTraitProvider));
+  group("For PowerWidget, ", () {
+    testWidgets('When loading, should show CircularProgressIndicator ',
+        (WidgetTester tester) async {
+      final mockPowerTraitProvider = MockPowerTraitProvider();
+      when(mockPowerTraitProvider.isBusy).thenReturn(true);
+      await tester.pumpWidget(createMaterialApp(mockPowerTraitProvider));
 
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
-    expect(find.byType(CupertinoSwitch), findsNothing);
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      expect(find.byType(CupertinoSwitch), findsNothing);
+    });
+
+    testWidgets('When an error occurs, should show some kind of error',
+        (WidgetTester tester) async {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      String errorMessage = "An error occurred";
+      final mockPowerTraitProvider = MockPowerTraitProvider();
+      when(mockPowerTraitProvider.isBusy).thenReturn(false);
+      when(mockPowerTraitProvider.isInErrorState).thenReturn(true);
+      when(mockPowerTraitProvider.getErrorMessage).thenReturn(errorMessage);
+
+      // tester.binding.scheduleFrame();
+      await tester.pumpWidget(createMaterialApp(mockPowerTraitProvider));
+
+      await tester.pump(const Duration(milliseconds: 750));
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.byIcon(Icons.error), findsOneWidget);
+      // expect(find.text(errorMessage), findsOneWidget);
+    });
+
+    testWidgets('Should show a Switch widget with On state if device is On',
+        (WidgetTester tester) async {
+      final mockPowerTraitProvider = MockPowerTraitProvider();
+      when(mockPowerTraitProvider.isBusy).thenReturn(false);
+      when(mockPowerTraitProvider.isInErrorState).thenReturn(false);
+      when(mockPowerTraitProvider.getOnOffState).thenReturn(false);
+
+      await tester.pumpWidget(createMaterialApp(mockPowerTraitProvider));
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.byType(CupertinoSwitch), findsOneWidget);
+      expect(
+          find.byWidgetPredicate(
+              (widget) => widget is CupertinoSwitch && widget.value == false,
+              description: 'Switch is Off'),
+          findsOneWidget);
+    });
+
+    testWidgets('Should show a Switch widget with Off state if device is Off',
+        (WidgetTester tester) async {
+      final mockPowerTraitProvider = MockPowerTraitProvider();
+      when(mockPowerTraitProvider.isBusy).thenReturn(false);
+      when(mockPowerTraitProvider.isInErrorState).thenReturn(false);
+      when(mockPowerTraitProvider.getOnOffState).thenReturn(true);
+
+      await tester.pumpWidget(createMaterialApp(mockPowerTraitProvider));
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.byType(CupertinoSwitch), findsOneWidget);
+      expect(
+          find.byWidgetPredicate(
+              (widget) => widget is CupertinoSwitch && widget.value == true,
+              description: 'Switch is On'),
+          findsOneWidget);
+    });
   });
 }
