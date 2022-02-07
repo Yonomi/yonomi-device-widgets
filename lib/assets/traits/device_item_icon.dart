ce5483b59a7b8f0f574f17318b0c3b8d75a9d11a
Rigoberto L. Perez <rigoberto.perez@allegion.com>
2022-02-07 11:57:16 -0500

    refactor(device_item_icon.dart): Refactor out some logic

--- lib/assets/traits/device_item_icon.dart
+++ lib/assets/traits/device_item_icon.dart
@@ -5,36 +5,38 @@ import 'package:yonomi_device_widgets/assets/traits/battery_level_icon.dart';
 import 'package:yonomi_device_widgets/assets/traits/thermostat_icon.dart';
 import 'package:yonomi_device_widgets/assets/traits/unknown_item_icon.dart';
 import 'package:yonomi_device_widgets/ui/widget_style_constants.dart';
-import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart' as sdk;
 
 class DeviceItemIcon {
-  static Widget getIcon(List<Trait> traits) {
+  static Widget getIcon(List<sdk.Trait> traits) {
     try {
-      Trait determiningTrait = traits[0];
+      sdk.Trait determiningTrait = traits[0];
       switch (determiningTrait.runtimeType) {
-        case LockTrait:
-          return LockIcon(determiningTrait.states
-              .firstWhere((state) => state is IsLocked)
-              .value);
-        case ThermostatTrait:
+        case sdk.LockTrait:
+          return LockIcon(
+              findIconStateValue<sdk.IsLocked, bool>(determiningTrait.states));
+        case sdk.ThermostatTrait:
           return ThermostatIcon(
-              thermostatState: determiningTrait.states
-                  .firstWhere((state) => state is TargetTemperature)
-                  .value);
-        case PowerTrait:
-          return PowerItemIcon(determiningTrait.states
-              .firstWhere((state) => state is IsOnOff)
-              .value);
-        case BatteryLevelTrait:
-          return BatteryLevelIcon(determiningTrait.states
-              .firstWhere((state) => state is BatteryLevel)
-              .value);
+              thermostatState:
+                  findIconStateValue<sdk.TargetTemperature, double?>(
+                      determiningTrait.states));
+        case sdk.PowerTrait:
+          return PowerItemIcon(
+              findIconStateValue<sdk.IsOnOff, bool>(determiningTrait.states));
+        case sdk.BatteryLevelTrait:
+          return BatteryLevelIcon(findIconStateValue<sdk.BatteryLevel, int>(
+              determiningTrait.states));
         default:
           return UnknownItemIcon();
       }
     } catch (e) {
       return UnknownItemIcon();
     }
+  }
+
+  static S findIconStateValue<T extends sdk.State<dynamic>, S>(
+      Set<sdk.State> states) {
+    return states.firstWhere((state) => state is T).value;
   }
 
   static buildLockUnlockIcon(bool isLocked,
