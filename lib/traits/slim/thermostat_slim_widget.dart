300f15a63a1f83adf5a6e9ba966fb86be5d68a52
Rigoberto L. Perez <rigoberto.perez@allegion.com>
2022-02-10 17:14:27 -0500

    refactor: Refactor code to make more readable and add display of ambient temp

--- lib/traits/slim/thermostat_slim_widget.dart
+++ lib/traits/slim/thermostat_slim_widget.dart
@@ -4,6 +4,7 @@ import 'package:yonomi_device_widgets/providers/thermostat_provider.dart';
 import 'package:yonomi_device_widgets/traits/slim/base_slim_widget.dart';
 import 'package:yonomi_device_widgets/ui/widget_style_constants.dart';
 import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
+import 'package:built_value/built_value.dart';
 
 class ThermostatSlimWidget extends BaseSlimWidget {
   ThermostatSlimWidget(ThermostatProvider thermostatProvider,
@@ -18,102 +19,84 @@ class ThermostatSlimWidget extends BaseSlimWidget {
                 style: TextStyle(
                     fontSize: 20, color: WidgetStyleConstants.darkTextColor)),
             backgroundColor: backgroundColor,
-            createContent: (context) => Column(
-                  children: [
-                    Wrap(
-                        spacing: 5.0,
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                'Modes: ',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headline6
-                                    ?.copyWith(
-                                        color:
-                                            WidgetStyleConstants.darkTextColor),
-                              )),
-                          ...List<Widget>.generate(
-                            thermostatProvider
-                                .getAvailableThermostatModes.length,
-                            (int index) {
-                              final availableModes = thermostatProvider
-                                  .getAvailableThermostatModes
-                                  .toList();
-                              return ChoiceChip(
-                                visualDensity: VisualDensity.comfortable,
-                                padding:
-                                    EdgeInsets.only(right: -1.0, left: -1.0),
-                                labelPadding:
-                                    EdgeInsets.only(right: 5.0, left: 5.0),
-                                label: Text('${availableModes[index].name}'),
-                                selected: thermostatProvider.getModeState ==
-                                    thermostatProvider
-                                        .getAvailableThermostatModes
-                                        .toList()[index],
-                                onSelected: (bool selected) {
-                                  if (selected) {
-                                    thermostatProvider.setThermostatMode(
-                                        thermostatProvider.deviceDetail?.id ??
-                                            '',
-                                        availableModes[index]);
-                                  }
-                                },
-                                key: Key(
-                                    'modeChip-${availableModes[index].name}'),
-                              );
-                            },
-                          ).toList(),
-                        ]),
-                    Wrap(
-                      spacing: 5.0,
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              'Fan Modes: ',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline6
-                                  ?.copyWith(
-                                      color:
-                                          WidgetStyleConstants.darkTextColor),
-                            )),
-                        ...List<Widget>.generate(
-                          thermostatProvider.getAvailableFanModes.length,
-                          (int index) {
-                            final availableFanModes = thermostatProvider
-                                .getAvailableFanModes
-                                .toList();
-                            return ChoiceChip(
-                                visualDensity: VisualDensity.comfortable,
-                                padding:
-                                    EdgeInsets.only(right: -1.0, left: -1.0),
-                                labelPadding:
-                                    EdgeInsets.only(right: 5.0, left: 5.0),
-                                label: Text('${availableFanModes[index].name}'),
-                                selected: thermostatProvider.getFanModeState ==
-                                    thermostatProvider.getAvailableFanModes
-                                        .toList()[index],
-                                onSelected: (bool selected) {
-                                  if (selected) {
-                                    thermostatProvider.setFanMode(
-                                        thermostatProvider.deviceDetail?.id ??
-                                            '',
-                                        thermostatProvider.getAvailableFanModes
-                                            .toList()[index]);
-                                  }
-                                },
-                                key: Key(
-                                    'fanModeChip-${availableFanModes[index].name}'));
-                          },
-                        ).toList(),
-                      ],
-                    ),
-                  ],
-                ),
+            createContent: (context) => _content(context, thermostatProvider),
             key: key);
+
+  static Widget _content(
+      BuildContext context, ThermostatProvider thermostatProvider) {
+    return Column(children: [
+      _chipList<AvailableThermostatMode>(
+          context,
+          'Modes: ',
+          thermostatProvider,
+          thermostatProvider.getAvailableThermostatModes.toList(),
+          thermostatProvider.getModeState,
+          thermostatProvider.setThermostatMode,
+          'modeChip'),
+      _chipList<AvailableFanMode>(
+          context,
+          'Fan Modes: ',
+          thermostatProvider,
+          thermostatProvider.getAvailableFanModes.toList(),
+          thermostatProvider.getFanModeState,
+          thermostatProvider.setFanMode,
+          'fanModeChip'),
+    ]);
+  }
+
+  static Widget _chipList<T extends EnumClass>(
+      BuildContext context,
+      String label,
+      ThermostatProvider thermostatProvider,
+      List<T> values,
+      T selectedValue,
+      Function clickAction,
+      String keyName) {
+    return Wrap(
+        spacing: 5.0,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Modes: ',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    ?.copyWith(color: WidgetStyleConstants.darkTextColor),
+              )),
+          ...List<Widget>.generate(
+            values.length,
+            (int index) {
+              return ChoiceChip(
+                  visualDensity: VisualDensity.comfortable,
+                  padding: EdgeInsets.only(right: -1.0, left: -1.0),
+                  labelPadding: EdgeInsets.only(right: 5.0, left: 5.0),
+                  label: Text('${values[index].name}'),
+                  selected: selectedValue == values[index],
+                  onSelected: (bool selected) {
+                    if (selected) {
+                      _doTap(thermostatProvider, values[index]);
+                    }
+                  },
+                  key: Key('$keyName-${values[index].name}'));
+            },
+          ).toList()
+        ]);
+  }
+
+  static Future<void> _doTap<T>(
+      ThermostatProvider thermostatProvider, T value) {
+    final deviceId = thermostatProvider.deviceDetail?.id ?? '';
+    switch (T) {
+      case AvailableThermostatMode:
+        return thermostatProvider.setThermostatMode(
+            deviceId, value as AvailableThermostatMode);
+      case AvailableFanMode:
+        return thermostatProvider.setFanMode(
+            deviceId, value as AvailableFanMode);
+      default:
+        return Future.value();
+    }
+  }
 }
