a9af751e247c524aaaab8e22ae5837f8037aaabf
Sergio E <ses110@gmail.com>
2022-04-20 13:02:50 -0600

    feat(color_temperature): introduce ColorTemperatureWidget

---
+++ test/mixins/color_temperature_testing.dart
@@ -0,0 +1,63 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:yonomi_device_widgets/providers/color_temperature_provider.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
+
+import 'color_temperature_testing.mocks.dart';
+
+@GenerateMocks([ColorTemperatureProvider])
+mixin ColorTemperatureTesting {
+  MockColorTemperatureProvider mockColorTemperatureProvider(
+      TestColorTemperatureDevice device,
+      {bool isBusy = false,
+      bool isLoading = false,
+      bool isInErrorState = false,
+      String errorMessage = 'Some mock error occurred',
+      bool isPerformingAction = false}) {
+    final mockColorTemperatureProvider = MockColorTemperatureProvider();
+
+    when(mockColorTemperatureProvider.displayName)
+        .thenReturn(device.displayName);
+    when(mockColorTemperatureProvider.isLoading).thenReturn(isLoading);
+    when(mockColorTemperatureProvider.isPerformingAction)
+        .thenReturn(isPerformingAction);
+    when(mockColorTemperatureProvider.isBusy).thenReturn(isBusy);
+    when(mockColorTemperatureProvider.isInErrorState)
+        .thenReturn(isInErrorState);
+    when(mockColorTemperatureProvider.getErrorMessage).thenReturn(errorMessage);
+    when(mockColorTemperatureProvider.deviceDetail).thenReturn(device);
+
+    when(mockColorTemperatureProvider.getColorTemperatureState)
+        .thenReturn(device.colorTemperature);
+
+    when(mockColorTemperatureProvider.setColorTemperatureAction(any))
+        .thenAnswer((_) => Future.value());
+
+    return mockColorTemperatureProvider;
+  }
+}
+
+class TestColorTemperatureDevice extends Device {
+  final int? colorTemperature;
+
+  TestColorTemperatureDevice(Device device, {this.colorTemperature = 3500})
+      : super(
+            device.id,
+            device.displayName,
+            device.description,
+            device.manufacturerName,
+            device.model,
+            device.serialNumber,
+            device.createdAt,
+            device.updatedAt, [
+          ColorTemperatureTrait(
+            {ColorTemperature(colorTemperature)},
+            {SupportedColorTemperatureRange(IntRange(min: 2000, max: 7000))},
+          ),
+          ...device.traits.where((t) => t.runtimeType != ColorTemperatureTrait)
+        ]);
+
+  TestColorTemperatureDevice withColorTemperature(int? colorTemperature) {
+    return TestColorTemperatureDevice(this, colorTemperature: colorTemperature);
+  }
+}
