54c5e7fc8f76b36a890c9215f6252693959e6de1
Sergio E <ses110@gmail.com>
2022-04-22 11:55:28 -0600

    refactor(color_temp_icon): create default icon field

--- lib/assets/traits/color_temperature_icon.dart
+++ lib/assets/traits/color_temperature_icon.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:yonomi_device_widgets/ui/widget_style_constants.dart';
 
 class ColorTemperatureIcon extends Icon {
+  static const DEFAULT_ICON = BootstrapIcons.lightbulb;
+
   ColorTemperatureIcon(int colorTemperature,
       {size = WidgetStyleConstants.defaultDeviceIconSize,
       color = WidgetStyleConstants.deviceIconColor,
       Key? key})
-      : super(BootstrapIcons.lightbulb, key: key, size: size, color: color);
+      : super(DEFAULT_ICON, key: key, size: size, color: color);
 }
