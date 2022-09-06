5d7b3e6ba7fa7200d543ed1c990aaeb659f34509
Sergio E <ses110@gmail.com>
2022-09-06 14:44:53 -0600

    feat(pin_code_widget): new pin code widget

---
+++ lib/traits/pin_code_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:yonomi_device_widgets/mixins/toast_notifications.dart';
+import 'package:yonomi_device_widgets/providers/pin_code_provider.dart';
+import 'package:yonomi_device_widgets/ui/widget_style_constants.dart';
+
+class PinCodeWidget extends StatelessWidget with ToastNotifications {
+  final PinCodeProvider _pinCodeProvider;
+  late final Color _iconColor;
+  late final Color _textColor;
+  late final double _iconSize;
+
+  PinCodeWidget(this._pinCodeProvider,
+      {Color iconColor = WidgetStyleConstants.deviceDetailIconColorActive,
+      Color textColor = WidgetStyleConstants.darkTextColor,
+      double iconSize = 100.0,
+      Key? key})
+      : super(key: key) {
+    this._iconColor = iconColor;
+    this._textColor = textColor;
+    this._iconSize = iconSize;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_pinCodeProvider.isBusy) {
+      return SizedBox(
+          child: CircularProgressIndicator(), height: 20, width: 20);
+    } else if (_pinCodeProvider.isInErrorState) {
+      showToast(context, _pinCodeProvider.getErrorMessage);
+      return Icon(Icons.error);
+    } else {
+      return Text("Pin Code Widget");
+    }
+  }
+}
