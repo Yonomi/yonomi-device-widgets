805bd15c725d62ecd50ada48fa83172f4583f2d2
Sergio E <ses110@gmail.com>
2022-09-06 17:39:34 -0600

    refactor(pin_code_slim_widget): update icons

--- lib/traits/slim/pin_code_slim_widget.dart
+++ lib/traits/slim/pin_code_slim_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:yonomi_device_widgets/assets/traits/pin_code_icon.dart';
 import 'package:yonomi_device_widgets/providers/pin_code_provider.dart';
 import 'package:yonomi_device_widgets/traits/slim/base_slim_widget.dart';
+import 'package:yonomi_device_widgets/ui/string_constants.dart';
 import 'package:yonomi_device_widgets/ui/widget_style_constants.dart';
 
 class PinCodeSlimWidget extends BaseSlimWidget {
@@ -14,12 +15,15 @@ class PinCodeSlimWidget extends BaseSlimWidget {
             leftIcon: (pinCodeProvider.isBusy)
                 ? SizedBox(
                     child: CircularProgressIndicator(), height: 20, width: 20)
-                : PinCodeIcon(),
-            headerText: const Text('Manage PIN Codes',
+                : PinCodeIcon(size: 30.0),
+            headerText: const Text(StringConstants.MANAGE_PIN_CODES,
                 style: TextStyle(
                     fontSize: 20, color: WidgetStyleConstants.darkTextColor)),
             rightIcon: IconButton(
-                icon: const Icon(BootstrapIcons.chevron_bar_right),
+                icon: const Icon(
+                  BootstrapIcons.chevron_right,
+                  color: Colors.black,
+                ),
                 onPressed: () => null
                 // TODO: navigate to Manage Pin Codes screen
                 ),
--- lib/ui/string_constants.dart
+++ lib/ui/string_constants.dart
@@ -1,4 +1,5 @@
 class StringConstants {
   static const String AN_ERROR_OCCURRED = "An error occurred.";
   static const String DEVICE_IS_JAMMED = "The device is jammed.";
+  static const String MANAGE_PIN_CODES = "Manage PIN Codes";
 }
