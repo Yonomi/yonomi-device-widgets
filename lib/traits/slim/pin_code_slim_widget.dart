ced130cc198a6d3e32dd5dfe4f787337a816e0bf
Sergio E <ses110@gmail.com>
2022-09-07 18:33:58 -0600

    feat(pin_code_slim_widget): show bottom sheet on click

--- lib/traits/slim/pin_code_slim_widget.dart
+++ lib/traits/slim/pin_code_slim_widget.dart
@@ -1,11 +1,13 @@
 import 'package:bootstrap_icons/bootstrap_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:yonomi_device_widgets/assets/traits/pin_code_icon.dart';
 import 'package:yonomi_device_widgets/providers/pin_code_provider.dart';
 import 'package:yonomi_device_widgets/traits/slim/base_slim_widget.dart';
 import 'package:yonomi_device_widgets/ui/string_constants.dart';
 import 'package:yonomi_device_widgets/ui/widget_style_constants.dart';
+import 'package:yonomi_platform_sdk/src/repository/traits/pin_code_repository.dart';
 
 class PinCodeSlimWidget extends BaseSlimWidget {
   PinCodeSlimWidget(PinCodeProvider pinCodeProvider,
@@ -19,14 +21,42 @@ class PinCodeSlimWidget extends BaseSlimWidget {
             headerText: const Text(StringConstants.MANAGE_PIN_CODES,
                 style: TextStyle(
                     fontSize: 20, color: WidgetStyleConstants.darkTextColor)),
-            rightIcon: IconButton(
+            rightIcon: Builder(builder: (context) {
+              return IconButton(
                 icon: const Icon(
                   BootstrapIcons.chevron_right,
                   color: Colors.black,
                 ),
-                onPressed: () => null
-                // TODO: navigate to Manage Pin Codes screen
-                ),
+                onPressed: () => {
+                  showCupertinoModalBottomSheet(
+                    context: context,
+                    expand: true,
+                    enableDrag: true,
+                    builder: (context) => SingleChildScrollView(
+                      controller: ModalScrollController.of(context),
+                      child: Container(
+                        child:
+                            PinCodeList(pinCodeProvider.getPinCodeCredentials),
+                      ),
+                    ),
+                  )
+                },
+              );
+            }),
             backgroundColor: backgroundColor,
             key: key);
+}
+
+class PinCodeList extends StatelessWidget {
+  final List<PinCodeCredential>? pinCodeCredentials;
+
+  const PinCodeList(
+    this.pinCodeCredentials, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Pin Code 1');
+  }
 }
